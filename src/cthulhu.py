765d3863081d68ac8f781889d93ccdc9a989661d
Mike Friesen <mfriesen@gmail.com>
2013-12-26 19:13:56 -0600

    rewrote spawn/cthulhu startup scripts

---
+++ src/cthulhu.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import cherrypy
+
+from cherrypy.process.plugins import Monitor
+from tentacle.cthulhu.operation import querySpawns
+from tentacle.cthulhu import Root
+
+cherrypy.config.update({'server.thread_pool' : 10,
+                        'server.socket_port' : 8080,
+                        'tools.sessions.on' : True
+                       })
+
+config = {
+'/' : { 'tools.staticdir.root' : '/Users/slycer/Documents/workspace/python/tentacle/src/tentacle/cthulhu/webroot' },
+'/css' : { 'tools.staticdir.on': True, 'tools.staticdir.dir' : 'css' },
+'/js' : { 'tools.staticdir.on': True, 'tools.staticdir.dir' : 'js' },
+'/images' : { 'tools.staticdir.on': True, 'tools.staticdir.dir' : 'images' },
+}
+
+cherrypy.tree.mount(Root(), "/", config=config)
+
+Monitor(cherrypy.engine, querySpawns, frequency=5).subscribe()
+
+cherrypy.engine.start()
+cherrypy.engine.block()
